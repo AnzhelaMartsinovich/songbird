7a7ecd69bdc71fecb6b0914cfc9e413f9b516ff6
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2020-02-10 20:34:11 +0300

    feat: add audio and adaptation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import Question from './components/Question/Question'
 import Wrapper from './components/Wrapper'
 import NextBtn from './components/Next/NextBtn'
+import './media.css';
 
 function App() {
   return (
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 import bird from './../../img/bird.jpg';
+// import AudioPlayer from "react-h5-audio-player";
 
 function Question() {
 	return (
@@ -8,7 +9,9 @@ function Question() {
 			<img src={bird} alt="bird" className="question-bird" />
 			<div className="question-right">
 				<h3 className="question-title">Какая птица издает звуки?</h3>
-				<audio src="#"></audio>
+
+				<audio controls="controls" src="https://24navo.com/lessons/html_list/i/m/4z.mp3" className="question__audio" />
+
 			</div>
 		</div>
 	)
