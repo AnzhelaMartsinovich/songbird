a8ce8402a0e733106cd4fe59cced952349c224bd
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2020-02-11 19:23:55 +0300

    feat: add display none/block for descr

--- src/components/Descr/Descr.js
+++ src/components/Descr/Descr.js
@@ -1,36 +1,81 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './style.scss';
 
-function Descr({ birds, selectedBird }) {
-	birds = birds[0];
+class Descr extends Component {
+	constructor(props) {
+		super(props)
 
-	let allInformation = birds.find(bird => bird.name === selectedBird);
+		this.state = {
+			isOpen: false
+		}
+	}
 
-	// const [display, setDisplay] = useState('none');
-	return (
-		<div className="descr">
-			<div className="flex-wrap">
-				<div className="descr-titles">
-					<h3 className="descr-title">
-						{selectedBird === null ? 'Правила викторины:' : selectedBird}
-					</h3>
+	render() {
+		let allInformation = this.props.birds[0].find(bird => bird.name === this.props.selectedBird);
 
-					<h5 className="descr-subtitle">
-						{allInformation === undefined ? '' : allInformation.species}
-					</h5>
+		return (
+			<div className="descr">
+				<div className="flex-wrap">
+					<div className="descr-titles">
+						<h3 className="descr-title">
+							{this.props.selectedBird === null ? 'Правила викторины:' : this.props.selectedBird}
+						</h3>
 
-					<audio controls="controls" src={allInformation === undefined ? '' : allInformation.audio} className="descr__audio" />
+						<h5 className="descr-subtitle">
+							{allInformation === undefined ? '' : allInformation.species}
+						</h5>
+
+						<audio controls="controls" src={allInformation === undefined ? '' : allInformation.audio} className="descr__audio" style={{ display: allInformation === undefined ? 'none' : 'block' }}/>
+					</div>
+
+					<img className="descr__bird-img" src={allInformation === undefined ? '' : allInformation.image} alt="bird" style={{ display: allInformation === undefined ? 'none' : 'block' }}/>
 				</div>
 
-				<img className="descr__bird-img" src={allInformation === undefined ? '' : allInformation.image} alt="bird" />
+				<p className="descr-text" style={{ width: allInformation === undefined ? '70%' : '100%', marginTop: allInformation === undefined ? '0' : '20px' }}>
+					{allInformation === undefined ? 'Сначала послушайте пение птицы, используя плеер, а затем выберите один из вариантов' : allInformation.description}
+				</p>
 			</div>
+		)
+	}
 
-			<p className="descr-text">
-				{allInformation === undefined ? 'Сначала послушайте пение птицы, используя плеер, а затем выберите один из вариантов' : allInformation.description}
-			</p>
-
-		</div>
-	)
 }
 
 export default Descr
+
+
+// import React, { useState } from 'react';
+// import './style.scss';
+
+// function Descr({ birds, selectedBird }) {
+// 	birds = birds[0];
+
+// 	let allInformation = birds.find(bird => bird.name === selectedBird);
+
+// 	// const [display, setDisplay] = useState('none');
+// 	return (
+// 		<div className="descr">
+// 			<div className="flex-wrap">
+// 				<div className="descr-titles">
+// 					<h3 className="descr-title">
+// 						{selectedBird === null ? 'Правила викторины:' : selectedBird}
+// 					</h3>
+
+// 					<h5 className="descr-subtitle">
+// 						{allInformation === undefined ? '' : allInformation.species}
+// 					</h5>
+
+// 					<audio controls="controls" src={allInformation === undefined ? '' : allInformation.audio} className="descr__audio" />
+// 				</div>
+
+// 				<img className="descr__bird-img" src={allInformation === undefined ? '' : allInformation.image} alt="bird" />
+// 			</div>
+
+// 			<p className="descr-text">
+// 				{allInformation === undefined ? 'Сначала послушайте пение птицы, используя плеер, а затем выберите один из вариантов' : allInformation.description}
+// 			</p>
+
+// 		</div>
+// 	)
+// }
+
+// export default Descr
